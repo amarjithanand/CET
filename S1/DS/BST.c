d4c4b63651b98b51f3ca0cfe45f743beda00863a
amarjithanand <amarjithanand017@gmail.com>
2025-10-23 08:12:22 +0530

    fix: correct insert_bst function logic and add missing newline

--- S1/DS/BST.c
+++ S1/DS/BST.c
@@ -2,5 +2,28 @@
 
 struct node{
     int data;
-    
+    struct node *right,*left;
+};
+struct bst{
+    struct node *root;
+};
+void insert_bst(struct node *t){
+    int value;
+    struct node *ptr = t;
+    printf("Enter the data you want to insert :");
+    scanf("%d",&value);
+    if(ptr->data==NULL){
+        ptr->data=value;
+    }
+    else if(value < ptr->data){
+        ptr = ptr->left;
+        insert_bst(ptr);
+    }
+    else if(value > ptr->data){
+        ptr = ptr -> right;
+        insert_bst(ptr);
+    }
+    else{
+        printf("The data inserted already existed in tree!!");
+    }
 }
