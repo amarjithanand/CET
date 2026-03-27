713264a87179a61edd32fd1508caae73f07638ec
amarjithanand <amarjithanand17@gmail.com>
2026-03-27 09:59:56 +0530

    Refactor mongo1.py by commenting out unused code and updating marks for students with marks greater than 30

--- S2/ADBMS/mongo1.py
+++ S2/ADBMS/mongo1.py
@@ -2,12 +2,16 @@ import pymongo
 conn = pymongo.MongoClient("mongodb://localhost:27017")
 db = conn["students"]
 col = db["mark"]
-data  = {
-    "rollno":4,
-    "name":"Amarjith",
-    "marks" : 97
-}
-col.insert_one(data)
-for doc in col.find({"marks": {"$gt":95}}):
-    print(doc["name"])
+# data  = {
+#     "rollno":4,
+#     "name":"Amarjith",
+#     "marks" : 97
+# }
+# col.insert_one(data)
+# for doc in col.find({"name": {"$regex":"h$"}}):
+#     print(doc["name"])
 
+col.update_many(
+    { "mark": { "$gt": 30 } },
+    { "$inc": { "mark": 1 } }
+)
